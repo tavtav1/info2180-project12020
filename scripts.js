f077186574a361921d85a903a483d15ede56c2e2
Tavian Hall <tavian.hall@hotmail.com>
2020-11-09 23:57:42 -0500

    Almost completed

--- scripts.js
+++ scripts.js
@@ -1,1 +1,27 @@
 /* Add your JavaScript to this file */
+
+//"use strict";
+
+
+function message() {
+    "use strict";
+    var newbutton, pattern, email;
+    newbutton = document.getElementsByTagName("button")[0];
+    
+    pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+    
+    newbutton.addEventListener("click", function (f) {
+        email = document.getElementById("email").value;
+        f.preventDefault();
+        
+        if (email.match(pattern)) {
+            document.getElementsByClassName("message")[0].innerHTML = `Thank you! Your email address ${email} has been added to our mailing list`;
+                                } //end of if statement   
+        
+        else{
+            document.getElementsByClassName("message")[0].innerHTML = "Please Input Valid Email Address";
+            } //end of else statement
+  
+                                                }
+                         ); //end of click function
+                } //end of function message
